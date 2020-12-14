69ff7f3ab6a85e904a123431847cead5b963f7be
shiliang26 <1447813730@qq.com>
2020-12-15 01:07:51 +0800

    fix num_timesteps

--- cs285/infrastructure/dqn_utils.py
+++ cs285/infrastructure/dqn_utils.py
@@ -42,7 +42,7 @@ def get_env_kwargs(env_name):
             'learning_starts': 50000,
             'target_update_freq': 10000,
             'replay_buffer_size': int(1e6),
-            'num_timesteps': int(2e8),
+            'num_timesteps': int(2000000),
             'q_func': create_atari_q_network,
             'learning_freq': 4,
             'grad_norm_clipping': 10,
